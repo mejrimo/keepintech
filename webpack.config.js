0a5f266fa70eedccfc68d330a7ff991afe450ff1
mejrimo <mejrimo.dev@gmail.com>
2023-01-22 03:48:49 +0100

    Set up

---
+++ webpack.config.js
@@ -0,0 +1,127 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : 'style-loader';
+
+const config = {
+  entry: './src/assets/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'img/[name]-[hash][ext]',
+    clean: true,
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+
+    // Add your plugins here
+    new Dotenv({
+      systemVars: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+
+      // Add your rules for custom modules here
+      {
+        test: /\.(s[ac]ss)$/,
+        use: [
+          {
+            loader: stylesHandler,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [require('autoprefixer')],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name]-[hash][ext]',
+        },
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
